0f460be5210601ea534b9a6652ea0883b2509fca
mmolloy <mmolloy88@outlook.com>
2020-03-10 16:18:49 +1100

    added new lines to code. more prompts, will leiminate DRY violations when the core is working properly

--- core.rb
+++ core.rb
@@ -3,10 +3,14 @@ require "tty-prompt"
 require "tty-box"
 
 prompt = TTY::Prompt.new(enable_color: true)
+pastel = Pastel.new
 
-def diary_entry
 
-end
+    def diary_entry
+
+    end
+
+
 
 print TTY::Box.frame("
 Welcome to the terminal diary!
@@ -15,7 +19,38 @@ note: this is for subjective use, you can choose to
 share your thoughts if you'd like!!")
 
 
-prompt.keypress("Press space to continue", keys: [:space])
+pastel.blue(prompt.keypress("Press space to continue
+", 
+keys: [:space],))
+
+print "We'll start with 3 basic yes or no questions.
+"
+
+    prompt.yes?(
+        "Did you put aside enough time for yourself today? ")
+
+    prompt.yes?(
+        "Was today a stressful day? ")
+
+    prompt.yes?(
+        "Did you accomplish what you wanted today? ")
+
+print "thank you for your feedback!"
+
+
+prompt.keypress(
+"Press space to continue", 
+keys: [:space])
+
+
+
+    prompt.ask("
+        On a scale of 1 - 10 how would you rate you day? 
+        ")
+
+
+
+
 
 
 
