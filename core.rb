2ee301e24f993bc1f1e6448c3f3175763ae8d414
mmolloy <mmolloy88@outlook.com>
2020-03-11 11:45:17 +1100

    filled out diary_entry area, need to cycle back to diary entry section after prompt

--- core.rb
+++ core.rb
@@ -7,10 +7,6 @@ pastel = Pastel.new
 
 diary_entry = true
 
-    def diary_entry
-
-    end
-
 
 
 print TTY::Box.frame("
@@ -25,8 +21,10 @@ pastel.blue(prompt.keypress("Press space to continue
 ", 
 keys: [:space],))
 
-print "We'll start with 3 basic yes or no questions.
+print pastel.blue"We'll start with 3 basic yes or no questions.
 "
+
+# save entries to the below responses
 
     prompt.yes?(
         "Did you put aside enough time for yourself today? ")
@@ -37,56 +35,51 @@ print "We'll start with 3 basic yes or no questions.
     prompt.yes?(
         "Did you accomplish what you wanted today? ")
 
-print "thank you for your feedback!"
+puts "thank you for your feedback!"
 
 
 prompt.keypress(
 "Press space to continue", 
 keys: [:space])
 
-def scale(input)
+input = prompt.ask("
+On a scale of 1 - 10 how would you rate you day? 
+").to_i
 
-    prompt.ask("
-        On a scale of 1 - 10 how would you rate you day? 
-        ").to_i
-       
 
-            if gets.chomp >= 7
-                print "That's great"
+            if input >= 7
+                puts "That's great!! "
                 # if response is 7 or greater return a simple "that's great"
                 # before next step
-            elsif gets.chomp < 3
-                print "perhaps it would be better to speak with your family or a professional"
+            elsif input < 3
+                puts "perhaps it would be better to speak with your family or a professional "
                 # if the response is lower than 3, suggest that maybe the user
                 # should speak to family, friends or a professional
             
-            else print "that's good to hear"
+            else puts "that's good to hear "
                 # if the response is between 4 and 7, put "you're doing ok"
        
             end
 
-        
-end
-
-scale(input)
 
 # return here if the user chooses to write another entry
+while(diary_entry == true)
 
-# print "thank you for your response, would you like to
-#         write about your day? it can be a word or 2, or 
-#         a full on essay, no pressure: "
+    print TTY::Box.frame(
+        "thank you for your responses, would you like to
+            write about your day? it can be a word or 2, or 
+            a full on essay, no pressure: ")
 
-
-
-# prompt.ask
-
-# prompt.ask "Would you like to write another entry? (y/n): "
-#     input = gets.chomp
-#     if input != "y"
-#         diary_entry = false
-#     end
+entry = gets.chomp
 
 
+
+# diary_entry = prompt.ask("
+#     would you like to make another entry? ")
+
+#         if diary_entry = "y"
+
+end
 
 
 
